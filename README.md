c1b5f32281a6572eedc8c729fd347bb5974fcb21
Emmanuel Lampe <mail@emmanuel-lampe.de>
2021-12-25 06:56:12 +0000

    added IW4MAdmin install script

---
+++ README.md
@@ -0,0 +1,3 @@
+# plutonium t6 servers in docker
+
+Here is a setup how to run t6servers and iw4admin in docker nicely
---
+++ scripts/install-admin.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+WORKING_DIR=$(pwd)
+INSTALL_DIR="$WORKING_DIR"/admin2
+
+rm -rf $INSTALL_DIR
+mkdir -p $INSTALL_DIR
+
+cd $INSTALL_DIR
+
+curl -s https://api.github.com/repos/RaidMax/IW4M-Admin/releases \
+	| grep -m 1 "browser_download_url" \
+	| cut -d : -f 2,3 \
+	| tr -d \" \
+	| wget -qi -
+
+unzip IW4MAdmin-*.zip
+rm -f IW4MAdmin-*.zip
+
+cp ../docker/Dockerfile.admin ./Dockerfile
+docker build -t t6server_iw4madmin .
+cd $WORKING_DIR
