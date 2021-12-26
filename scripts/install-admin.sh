8f06dcdcdcfc170bfb482ea469d40506f39a36b7
Emmanuel Lampe <mail@emmanuel-lampe.de>
2021-12-26 03:24:47 +0100

    merged setup admin to installation

--- scripts/install-admin.sh
+++ scripts/install-admin.sh
@@ -2,6 +2,10 @@
 
 WORKING_DIR=$(pwd)
 INSTALL_DIR="$WORKING_DIR"/admin
+SERVERS_DIR="$WORKING_DIR"/servers
+LOGS_DIR="$SERVERS_DIR"/plutonium/storage/t6/logs/
+# Customize if you want
+PORT=1624
 
 rm -rf $INSTALL_DIR
 mkdir -p $INSTALL_DIR
@@ -19,4 +23,16 @@ rm -f IW4MAdmin-*.zip
 
 cp ../docker/Dockerfile.admin ./Dockerfile
 docker build -t t6server_iw4madmin .
+
+echo "Follow the installation, dont worry its normal that the tool fails to connect to the server"
+
+chmod +x "$INSTALL_DIR"/StartIW4MAdmin.sh
+CONTAINER_ID=$(docker run -itd --name t6server_iw4madmin \
+	-v $LOGS_DIR:/t6server-logs \
+	-v $INSTALL_DIR:/app \
+	-p 1624:$PORT/tcp \
+	t6server_iw4madmin)
+
+docker attach $CONTAINER_ID
+
 cd $WORKING_DIR
